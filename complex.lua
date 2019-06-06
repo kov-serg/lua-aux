b4f1f7532b7327855fa1fe11788fe2af6b7eeaf5
kov-serg <kov_serg@freemail.ru>
2019-06-07 00:58:45 +0300

    Update complex.lua

--- complex.lua
+++ complex.lua
@@ -1,55 +1,55 @@
-local complex_m={ __index={} }
-local complex_mt={ type='complex',scalar=true, __index={} }
-complex=setmetatable({},complex_m)
-complex_m.__call=function(m,x)
-	if type(x)=='number' then return setmetatable( { x,0 },complex_mt ) end
+local Complex={}
+local Complex_mt={ type='complex',scalar=true,__index=Complex }
+
+complex=function(x)
+	if type(x)=='number' then return setmetatable({x,0},Complex_mt) end
 	local mt=getmetatable(x)
-	if mt and mt.type~='complex' then error("unable to create complex number from "..mt.type) end
+	if mt and mt.type~=Complex_mt.type then error("unable to create complex number from "..mt.type) end
 	local r={ x[1] or 0,x[2] or 0 }
 	if type(r[1])~='number' or type(r[2])~='number' then error("need numbers") end
-	return setmetatable(r,complex_mt )
+	return setmetatable(r,Complex_mt)
 end
-complex_m.__index.conj=function(x) local t=complex(x) return complex{t[1],-t[2]} end
-complex_m.__index.re=function(x) local t=complex(x) return x[1] end
-complex_m.__index.im=function(x) local t=complex(x) return x[2] end
-complex_m.__index.norm=function(x) local t=complex(x) return t[1]*t[1]+t[2]*t[2] end
-complex_m.__index.len=function(x) local t=complex(x) return math.sqrt(t:norm()) end
-complex_m.__index.abs=function(x) local t=complex(x) return math.sqrt(t:norm()) end
-complex_m.__index.phase=function(x) local t=complex(x) 
+
+function Complex.re(x) local t=complex(x) return x[1] end
+function Complex.im(x) local t=complex(x) return x[2] end
+function Complex.len(x) local t=complex(x) return math.sqrt(t:norm()) end
+Complex.abs=Complex.len
+function Complex.conj(x) local t=complex(x) return complex{t[1],-t[2]} end
+function Complex.norm(x) local t=complex(x) return t[1]*t[1]+t[2]*t[2] end
+function Complex.phase(x) local t=complex(x) 
 	local a=math.atan(t[2],t[1])
 	return complex{ math.cos(a), math.sin(a) }
 end
-complex_m.__index.arg=function(x) local t=complex(x) return math.atan(t[2],t[1]) end
-complex_m.__index.exp=function(x) 
+function Complex.arg(x) local t=complex(x) return math.atan(t[2],t[1]) end
+function Complex.exp(x)
 	local z=complex(x)
 	local t=math.exp(z[1])
 	return complex{ t*math.cos(z[2]), t*math.sin(z[2]) }
 end
-complex_m.__index.log=function(x) 
+function Complex.log(x)
 	local z=complex(x)
 	return complex{ math.log(z:norm())/2, z:arg() }
 end
-complex_mt.__index=complex_m.__index
-complex_mt.__add=function(va,vb)
+function Complex_mt.__add(va,vb)
 	local a,b=complex(va),complex(vb)
 	return complex{ a[1]+b[1], a[2]+b[2] }
 end
-complex_mt.__unm=function(va)
+function Complex_mt.__unm(va)
 	local a=complex(va)
 	return complex{ -a[1], -a[2] }
 end
-complex_mt.__sub=function(va,vb)
+function Complex_mt.__sub(va,vb)
 	local a,b=complex(va),complex(vb)
 	return complex{ a[1]-b[1], a[2]-b[2] }
 end
-complex_mt.__mul=function(va,vb)
+function Complex_mt.__mul(va,vb)
 	-- n*c
 	-- c*n
 	-- c*c
 	-- c*v
 	-- c*m
 	local mt=getmetatable(vb)
-	if mt and mt.type~='complex' then
+	if mt and mt.type~=Complex_mt.type then
 		-- c*v
 		-- c*m
 		return vb:unary(function(vbi) return va*vbi end)
@@ -57,14 +57,14 @@ complex_mt.__mul=function(va,vb)
 	local a,b=complex(va),complex(vb)
 	return complex{ a[1]*b[1]-a[2]*b[2], a[2]*b[1]+a[1]*b[2] }
 end
-complex_mt.__div=function(va,vb)
+function Complex_mt.__div(va,vb)
 	-- n/c
 	-- c/n
 	-- c/c
 	-- c/v -- error
 	-- c/m = c*inv(m)
 	local mt=getmetatable(vb)
-	if mt and mt.type~='complex' then
+	if mt and mt.type~=Complex_mt.type then
 		-- c/v
 		-- c/m
 		return vb:scalar_ldiv(va)
@@ -73,7 +73,7 @@ complex_mt.__div=function(va,vb)
 	local d=b:norm()
 	return complex{ (a[1]*b[1]+a[2]*b[2])/d, (a[2]*b[1]-a[1]*b[2])/d }
 end
-complex_mt.__pow=function(va,vb)
+function Complex_mt.__pow(va,vb)
 	local a,b=complex(va),complex(vb)
 	local lnma=math.log(a:norm())/2
 	local arga=a:arg()
@@ -81,10 +81,10 @@ complex_mt.__pow=function(va,vb)
 	local f=b[1]*arga+b[2]*lnma
 	return complex{ s*math.cos(f), s*math.sin(f)  }
 end
-complex_m.__index.format_string="%.4g"
-complex_m.__index.format_prefix="complex"
-complex_mt.__tostring=function(z,fmt)
-	fmt=fmt or complex.format_string
-	local r=complex.format_prefix..'{' for i=1,2 do if i>1 then r=r..',' end r=r..string.format(fmt,z[i]) end
+Complex.format_string="%.4g"
+Complex.format_prefix=Complex_mt.type
+function Complex_mt:__tostring(fmt)
+	fmt=fmt or self.format_string
+	local r=self.format_prefix..'{' for i=1,2 do if i>1 then r=r..',' end r=r..string.format(fmt,self[i]) end
 	return r..'}'
 end
