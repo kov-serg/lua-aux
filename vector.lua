76158ba98e9e7fe9d5e4bf861a244e4ab59db75e
kov-serg <kov_serg@freemail.ru>
2019-06-07 00:59:30 +0300

    Update vector.lua

--- vector.lua
+++ vector.lua
@@ -1,8 +1,7 @@
-local vector_m={ __index={} }
-local vector_mt={ type='vector', __index={} }
-vector=setmetatable({},vector_m)
+local Vector={}
+local Vector_mt={ type='vector', __index=Vector }
 
-vector_m.__call=function(m,x)
+vector=function(x)
 	local r={}
 	local mt=getmetatable(x)
 	if type(x)=='number' or (mt and mt.scalar) then
@@ -10,9 +9,9 @@ vector_m.__call=function(m,x)
 	else
 		for k,v in ipairs(x) do r[k]=v end
 	end
-	return setmetatable(r,vector_mt)
+	return setmetatable(r,Vector_mt)
 end
-local tostring=function(v,fmt)
+local function tostring(v,fmt)
 	if type(v)=='number' then
 		return string.format(fmt,v)
 	elseif type(v)=='table' then
@@ -24,42 +23,41 @@ local tostring=function(v,fmt)
 	end
 	return type(v)
 end
-vector_m.__index.format_string="%.4g"
-vector_m.__index.format_prefix="vector"
-vector_mt.__tostring=function(v,fmt)
-	fmt=fmt or vector.format_string
-	local r=vector.format_prefix..'{'
-	for k,v in ipairs(v) do if k>1 then r=r..',' end r=r..tostring(v,fmt) end
+Vector.format_string="%.4g"
+Vector.format_prefix=Vector_mt.type
+function Vector_mt:__tostring(fmt)
+	fmt=fmt or self.format_string
+	local r=self.format_prefix..'{'
+	for k,v in ipairs(self) do if k>1 then r=r..',' end r=r..tostring(v,fmt) end
 	return r..'}'
 end
-local need_vector=function(va)
+local function need_vector(va)
 	local mt=getmetatable(va)
-	if mt and mt.type=='vector' then return end
+	if mt and mt.type==Vector_mt.type then return end
 	error("need vector",3)
 end
-vector_mt.__index.unary=function(a,fn)
-	local r={} for k,v in ipairs(a) do r[k]=fn(v) end
-	return setmetatable(r,vector_mt)
+function Vector:unary(fn)
+	local r={} for k,v in ipairs(self) do r[k]=fn(v) end
+	return setmetatable(r,Vector_mt)
 end
-vector_mt.__index.binary=function(a,b,fn)
-	need_vector(b)
+function Vector.binary(a,b,fn) need_vector(b)
 	local na,nb,r=#a,#b,{}
 	local nab=math.min(na,nb)
 	for i=1,nab do r[i]=fn(a[i],b[i]) end
  	for i=na+1,nb do r[i]=fn(0,b[i]) end
   	for i=nb+1,na do r[i]=fn(a[i],0) end
-	return setmetatable(r,vector_mt)
+	return setmetatable(r,Vector_mt)
 end
-vector_mt.__index.norm=function(va)
+function Vector:norm()
 	local s=0
-	for k,v in ipairs(va) do if type(v)=='number' then s=s+v*v else s=s+v:norm() end end
+	for k,v in ipairs(self) do if type(v)=='number' then s=s+v*v else s=s+v:norm() end end
 	return s
 end
-vector_mt.__index.len=function(va) return va:norm()^0.5 end
-vector_mt.__unm=function(va) return va:unary(function(x) return -x end) end
-vector_mt.__add=function(va,vb) return va:binary(vb,function(ai,bi) return ai+bi end) end
-vector_mt.__sub=function(va,vb) return va:binary(vb,function(ai,bi) return ai-bi end) end
-vector_mt.__mul=function(va,vb)
+function Vector:len() return self:norm()^0.5 end
+function Vector_mt.__unm(va) return va:unary(function(x) return -x end) end
+function Vector_mt.__add(va,vb) return va:binary(vb,function(ai,bi) return ai+bi end) end
+function Vector_mt.__sub(va,vb) return va:binary(vb,function(ai,bi) return ai-bi end) end
+function Vector_mt.__mul(va,vb)
 	-- n*v =v
 	-- v*n =v
 	-- v*c =v
@@ -68,7 +66,7 @@ vector_mt.__mul=function(va,vb)
 	if type(va)=='number' then return vb:unary(function(vbi) return va*vbi end) end
 	local mt=getmetatable(vb)
 	if type(vb)=='number' or (mt and mt.scalar) then return va:unary(function(vai) return vai*vb end) end
-	if mt and mt.type=='vector' then
+	if mt and mt.type==Vector_mt.type then
 		local s=0
 		local n=math.min(#va,#vb)
 		for i=1,n do
@@ -80,46 +78,42 @@ vector_mt.__mul=function(va,vb)
 	end
 	error("operation vector*"..mt.type.." not implemented")
 end
-vector_mt.__div=function(va,vb)
+function Vector_mt.__div(va,vb)
 	-- n/v =error
 	-- v/n =v
 	-- v/c =v
 	-- v/v =error
 	-- v/m =v*inv(m) ?
 	local mt=getmetatable(vb)
-	if mt and mt.type=='vector' then error("operation /vector not supported",2) end
+	if mt and mt.type==Vector_mt.type then error("operation /vector not supported",2) end
 	if type(vb)=='number' or (mt and mt.scalar) then 
 		return va:unary(function(vai) return vai/vb end)
 	end
 	error("vector/"..mt.type)
 end
-vector_m.__index.vmul3=function(a,b)
+function Vector.vmul3(a,b)
 	if #a~=3 or #b~=3 then error "need 3d vectors" end
 	return vector{ a[2]*b[3]-a[3]*b[2], a[3]*b[1]-a[1]*b[3], a[1]*b[2]-a[2]*b[1] }
 end
-vector_m.__index.ort2=function(a)
-	if #a~=2 then error "need 2d vector" end
-	return vector{ a[2],-a[1] }
+function Vector:ort2()
+	if #self~=2 then error "need 2d vector" end
+	return vector{ self[2],-self[1] }
 end
-vector_m.__index.dir=function(a)
-	return a/a:len()
+function Vector:dir()
+	return self/self:len()
 end
 
 --[[
 require "complex"
-
 i=complex{0,1}
 a=vector{3,4*i}
 x=vector{1,i,0}
 y=vector{0,1,0}
 z=vector{0,0,1}
 q=vector{1+i,1-i,i}
-
-
 --print(x,y,z,q)
 print( x*y,y*x,z*i,i*z )
 --print(vector{1,complex{1,1}}*complex{0,1})
 --print(complex{0,1}*vector{1,complex{1,1}})
 print( vector(complex{1,2}))
 ]]
-
