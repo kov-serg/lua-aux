f3f89a09fb6305a355938ab77b64bdd672dc48e2
kov-serg <kov_serg@freemail.ru>
2019-06-06 23:05:21 +0300

    Update pure.lua

--- pure.lua
+++ pure.lua
@@ -7,3 +7,16 @@ function isolated(s)
 	if type(s)=="string" then return pure(s) end
 	return function(body) return pure(body,s) end
 end
+
+function pure_check()
+	local i,r,n,v,f	
+	i=0	r={} f=debug.getinfo(2,"f").func
+	while true do
+		i=i+1 n,v=debug.getupvalue(f,i)
+		if n==nil then break end
+		if n~="_ENV" then table.insert(r,n) end
+	end
+	if #r>0 then
+		error("pure_check fail: you must define\n\tlocal "..table.concat(r,","),2)
+	end
+end
