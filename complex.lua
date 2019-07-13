d43276decd04921dcc82086a8cb21dc8cf3b31bf
kov-serg <kov_serg@freemail.ru>
2019-07-14 00:14:59 +0300

    reorganize

--- complex.lua
+++ complex.lua
@@ -1,35 +1,43 @@
 local Complex={}
 local Complex_mt={ type='complex',scalar=true,__index=Complex }
 
-complex=function(x)
-	if type(x)=='number' then return setmetatable({x,0},Complex_mt) end
-	local mt=getmetatable(x)
-	if mt and mt.type~=Complex_mt.type then error("unable to create complex number from "..mt.type) end
-	local r={ x[1] or 0,x[2] or 0 }
-	if type(r[1])~='number' or type(r[2])~='number' then error("need numbers") end
-	return setmetatable(r,Complex_mt)
-end
+complex=setmetatable({},{
+	__call=function(t,x)
+		if type(x)=='number' then return setmetatable({x,0},Complex_mt) end
+		local mt=getmetatable(x)
+		if mt and mt.type~=Complex_mt.type then error("unable to create complex number from "..mt.type) end
+		local r={ x[1] or 0,x[2] or 0 }
+		if type(r[1])~='number' or type(r[2])~='number' then error("need numbers") end
+		return setmetatable(r,Complex_mt)
+	end,
+})
 
-function Complex.re(x) local t=complex(x) return x[1] end
-function Complex.im(x) local t=complex(x) return x[2] end
-function Complex.len(x) local t=complex(x) return math.sqrt(t:norm()) end
-Complex.abs=Complex.len
-function Complex.conj(x) local t=complex(x) return complex{t[1],-t[2]} end
-function Complex.norm(x) local t=complex(x) return t[1]*t[1]+t[2]*t[2] end
-function Complex.phase(x) local t=complex(x) 
+function complex.re(x) local t=complex(x) return x[1] end
+function complex.im(x) local t=complex(x) return x[2] end
+function complex.abs(x) local t=complex(x) return math.sqrt(t:norm()) end       
+function complex.conj(x) local t=complex(x) return complex{t[1],-t[2]} end
+function complex.norm(x) local t=complex(x) return t[1]*t[1]+t[2]*t[2] end
+function complex.phase(x) local t=complex(x) 
 	local a=math.atan(t[2],t[1])
 	return complex{ math.cos(a), math.sin(a) }
 end
-function Complex.arg(x) local t=complex(x) return math.atan(t[2],t[1]) end
-function Complex.exp(x)
+function complex.arg(x) local t=complex(x) return math.atan(t[2],t[1]) end
+function complex.exp(x)
 	local z=complex(x)
 	local t=math.exp(z[1])
 	return complex{ t*math.cos(z[2]), t*math.sin(z[2]) }
 end
-function Complex.log(x)
+function complex.log(x)
 	local z=complex(x)
 	return complex{ math.log(z:norm())/2, z:arg() }
 end
+Complex.re=complex.re
+Complex.im=complex.im
+Complex.len=complex.abs
+Complex.conj=complex.conj
+Complex.norm=complex.norm
+Complex.phase=complex.phase
+Complex.arg=complex.arg
 function Complex_mt.__add(va,vb)
 	local a,b=complex(va),complex(vb)
 	return complex{ a[1]+b[1], a[2]+b[2] }
@@ -88,3 +96,5 @@ function Complex_mt:__tostring(fmt)
 	local r=self.format_prefix..'{' for i=1,2 do if i>1 then r=r..',' end r=r..string.format(fmt,self[i]) end
 	return r..'}'
 end
+
+return complex
